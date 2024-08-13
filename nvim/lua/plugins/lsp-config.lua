f9cbc66aa5772c1a91dd70ef13c2209a745dfd36
Renan Jahn <r.jahn@hotmail.com>
2024-08-13 23:40:24 +0200

    new nvim setup

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -47,6 +47,7 @@ return {
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set('n', "<leader>gD", vim.lsp.buf.declaration, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
     end,
