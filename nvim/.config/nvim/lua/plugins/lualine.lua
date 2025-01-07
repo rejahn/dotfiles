0f70adca0a5ca21f98dad0aa4e18600c3c995b82
Renan Jahn <r.jahn@hotmail.com>
2025-01-07 23:26:00 +0100

    nvim: update lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,15 +1,18 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "catppuccin-mocha",
-        sections = {
-          lualine_a = {
-            file = 1,
-          },
-        },
-      },
-    })
-  end,
-}
+     "nvim-lualine/lualine.nvim",
+     dependencies = { 'nvim-tree/nvim-web-devicons' },
+     config = function()
+       require("lualine").setup({
+         options = {
+           theme = "catppuccin-mocha",
+           component_separators = { left = ' ', right = ' ' },
+           section_separators = { left = ' ', right = ' ' },
+           sections = {
+             lualine_a = {
+               file = 1,
+             },
+           },
+         },
+       })
+     end,
+   }
